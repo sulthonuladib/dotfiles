3ed073d250f90520d87d570beee07c980a9348b3
sulthonuladib <adibcyber4rt@gmail.com>
2021-02-14 19:10:09 +0700

    Add: neovim, tmux, git, vim-plug installation on script

--- install.sh
+++ install.sh
@@ -1,56 +1,91 @@
 #!/usr/bin/bash
 
+# Config dir and files
 NVIM_CONFIG_DIR="$HOME/.config/nvim"
 NVIM_CONFIG_FILE="$NVIM_CONFIG_DIR/init.vim"
 COC_CONFIG_FILE="$NVIM_CONFIG_DIR/coc-settings.json"
 TMUX_CONFIG_FILE="$HOME/.tmux.conf"
 GIT_CONFIG_FILE="$HOME/.gitconfig"
+VIMPLUG_DESTINATION="$HOME/.local/share/nvim"
+
+# Binary or program installation
+NVIM_EXECUTABLE_FILES="$(which nvim)"
+TMUX_EXECUTABLE_FILES="$(which tmux)"
+GIT_EXECUTABLE_FILES="$(which git)"
+
+echo "Installing neovim pre-release"
+if [ -f $NVIM_EXECUTABLE_FILES ]; then
+    echo "neovim already installed no need to install"
+else
+    echo "Installing neovim from source"
+    git clone https://github.com/neovim/neovim;
+    make && sudo make install;
+fi
+
+echo "Installing vim-plug";
+echo "thanks to https://github.com/junegunn";
+if [ -d $VIMPLUG_DESTINATION ]; then
+    echo "vim-plug already installed";
+else
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+           https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+fi
+
+
+echo "Installing tmux";
+if [ -f $TMUX_EXECUTABLE_FILES ]; then
+    echo "tmux exist, skipping tmux";
+else
+    echo "Installing tmux";
+    sudo pacman -S tmux;
+fi
+
+echo "Installing git";
+if [ -f $GIT_EXECUTABLE_FILES ]; then
+    echo "git exist, skipping git";
+else
+    echo "Installing tmux";
+    sudo pacman -S git
+fi
 
 echo "Setting up https://github.com/sulthonuladib/dotfiles";
-echo -e "--------------------------------\n";
 
 echo "Creating neovim config directory";
 if [ -d $NVIM_CONFIG_DIR ]; then
     echo "Failed to create $NVIM_CONFIG_DIR folder exist";
 else
     echo "$NVIM_CONFIG_DIR created";
-    mkdir "/home/$(whoami)/.config/nvim";
-fi
-echo -e "--------------------------------\n";
+    mkdir "/home/$(whoami)/.config/nvim"; fi
 
 echo "Linking neovim config";
 if [ -f $NVIM_CONFIG_FILE ]; then
     echo "Failed to link $NVIM_CONFIG_FILE file exist";
 else
     echo "$NVIM_CONFIG_FILE successfully linked";
-    ln -s "$(pwd)/init.vim /home/$(whoami)/.config/nvim/init.vim";
+    ln -s "$(pwd)/init.vim" "/home/$(whoami)/.config/nvim/init.vim";
 fi
-echo -e "--------------------------------\n";
 
 echo "Linking neovim coc-settings plugin"
 if [ -f $COC_CONFIG_FILE ]; then
     echo "Failed to link $COC_CONFIG_FILE file exist";
 else
     echo "$COC_CONFIG_FILE successfully linked";
-    ln -s "$(pwd)/coc-settings.json /home/$(whoami)/.config/nvim/coc-settings.json";
+    ln -s "$(pwd)/coc-settings.json" "/home/$(whoami)/.config/nvim/coc-settings.json";
 fi
-echo -e "--------------------------------\n";
 
 echo "Linking tmux config file"
 if [ -f $TMUX_CONFIG_FILE ]; then
     echo "Failed to link $TMUX_CONFIG_FILE file exist";
 else
     echo "$TMUX_CONFIG_FILE successfully linked";
-    ln -s "$(pwd)/tmux.conf /home/$(whoami)/.tmux.conf";
+    ln -s "$(pwd)/tmux.conf" "/home/$(whoami)/.tmux.conf";
 fi
-echo -e "--------------------------------\n";
 
 echo "Linking gitconfig file"
 if [ -f $GIT_CONFIG_FILE ]; then
     echo "Failed to link $GIT_CONFIG_FILE file exist";
 else
     echo "$GIT_CONFIG_FILE successfully linked";
-    ln -s "$(pwd)/gitconfig /home/$(whoami)/.gitconfig";
+    ln -s "$(pwd)/gitconfig" "/home/$(whoami)/.gitconfig";
 fi
-echo -e "--------------------------------\n";
 
