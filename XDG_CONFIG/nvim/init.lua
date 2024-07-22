a47e3d48256253ae39105d25e0fb6cb8fd595a5a
sulthonuladib <adibcyber4rt@gmail.com>
2024-07-22 18:12:11 +0700

    chore: moved to https://github.com/sulthonuladib/config.nvim

--- XDG_CONFIG/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-require("sulthonuladib")
--- vim.cmd[[colorscheme tokyonight-night]]
--- vim.notify = require("notify")
-
-vim.cmd.colorscheme("tokyonight-storm")
-vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000", border = "orange" }
